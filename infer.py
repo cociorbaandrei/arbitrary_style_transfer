36642e57c3f95c895ee2e238d9ae4d956abfcef9
Maciej Pęśko <mpesko3031@gmail.com>
2019-05-27 16:38:13 -0400

    Add minor refactor

--- infer.py
+++ infer.py
@@ -7,9 +7,8 @@ from style_transfer_net import StyleTransferNet
 from utils import get_images, save_images
 
 
-def stylize(contents_path, styles_path, output_dir, encoder_path, model_path, 
+def stylize(contents_path, styles_path, output_dir, encoder_path, model_path,
             resize_height=None, resize_width=None, suffix=None):
-
     if isinstance(contents_path, str):
         contents_path = [contents_path]
     if isinstance(styles_path, str):
@@ -19,7 +18,7 @@ def stylize(contents_path, styles_path, output_dir, encoder_path, model_path,
         # build the dataflow graph
         content = tf.placeholder(
             tf.float32, shape=(1, None, None, 3), name='content')
-        style   = tf.placeholder(
+        style = tf.placeholder(
             tf.float32, shape=(1, None, None, 3), name='style')
 
         stn = StyleTransferNet(encoder_path)
@@ -35,19 +34,17 @@ def stylize(contents_path, styles_path, output_dir, encoder_path, model_path,
         outputs = []
         for content_path in contents_path:
 
-            content_img = get_images(content_path, 
-                height=resize_height, width=resize_width)
+            content_img = get_images(content_path,
+                                     height=resize_height, width=resize_width)
 
             for style_path in styles_path:
+                style_img = get_images(style_path)
 
-                style_img   = get_images(style_path)
-
-                result = sess.run(output_image, 
-                    feed_dict={content: content_img, style: style_img})
+                result = sess.run(output_image,
+                                  feed_dict={content: content_img, style: style_img})
 
                 outputs.append(result[0])
 
     save_images(outputs, contents_path, styles_path, output_dir, suffix=suffix)
 
     return outputs
-
