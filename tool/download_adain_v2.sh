2630a8c8d273baed810e959a205cc1d530c786fd
Maciej Pęśko <mpesko3031@gmail.com>
2019-05-27 16:21:02 -0400

    Add other AdaIN model

---
+++ tool/download_adain_v2.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+cd ../models && wget -c https://s3.amazonaws.com/xunhuang-public/adain/decoder.t7
+cd ../models && wget -c https://s3.amazonaws.com/xunhuang-public/adain/decoder-content-similar.t7
+cd ../models && wget -c https://s3.amazonaws.com/xunhuang-public/adain/vgg_normalised.t7
---
+++ tool/install_torchfile.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+git clone https://github.com/bshillingford/python-torchfile.git
+cd python-torchfile && python setup.py install
+rm -r python-torchfile
